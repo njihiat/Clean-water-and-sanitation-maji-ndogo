adb62e6ff73ea06a611690f0548c62c279557963
WANJIHIA NJUGUNA <njihiat254@gmail.com>
2025-06-19 17:32:30 +0300

    Update Maji_Ndogo_SQL_Code.sql

--- Maji_Ndogo_SQL_Code.sql
+++ Maji_Ndogo_SQL_Code.sql
@@ -19,8 +19,8 @@ SELECT * FROM water_source;
 SELECT * FROM employee;
 SELECT * FROM data_dictionary;
 
-/* Each table was given a descriptive name, The employee table contains records of employees who are given different tasks in this water project
-The visit table contains recordds of each visit made to each water source, the time spent in queue was recorded.
+/* Each table is given a descriptive name, The employee table contains records of employees who are given different tasks in this water project
+The visit table contains records of each visit made to each water source, the time spent in queue was recorded.
 The water_quality contains records of different water source quality.
 In the water source table, a water source can be a tap in home, broken or working, a well, a shared tap  or a river.
 All the description about the columns and how the table relates to each other is recorded in the data dictionary table */
